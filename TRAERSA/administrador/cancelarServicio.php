038ec54a895c145129c31c590a85273af23fc328
Plan de clase 1 <ximenaletran@emilianisomascos.edu.gt>
2026-06-23 00:32:20 -0500

    Update cancelarServicio.php
    comentarios

--- TRAERSA/administrador/cancelarServicio.php
+++ TRAERSA/administrador/cancelarServicio.php
@@ -1,9 +1,12 @@
 <?php
 
+// Incluir conexion a la base de datos
 require '../conexion.php';
 
+// Obtener el id del envio enviado por POST
 $id = $_POST['id'];
 
+// Consulta preparada para cambiar el estado a Cancelado
 $sql = "
 UPDATE envios
 SET estado = 'Cancelado'
@@ -12,12 +15,15 @@ WHERE id_envio = ?
 
 $stmt = $conn->prepare($sql);
 
+// Enlazar el id al parametro de la consulta
 $stmt->bind_param(
     "i",
     $id
 );
 
+// Ejecutar la actualizacion
 $stmt->execute();
 
+// Responder con ok si todo salio bien
 echo "ok";
 ?>
