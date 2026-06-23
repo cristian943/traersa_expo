eb4fffe053d46da38bf12cc03963a305ba8a9ed9
Plan de clase 1 <ximenaletran@emilianisomascos.edu.gt>
2026-06-23 00:26:46 -0500

    Update actualizar_categoria.php
    comentarios

--- TRAERSA/administrador/actualizar_categoria.php
+++ TRAERSA/administrador/actualizar_categoria.php
@@ -1,6 +1,8 @@
 <?php
+// Incluimos la conexion con la base de datos
 include("../conexion.php");
 
+// Recibir los datos enviados desde el formulario
 $id = $_POST['id'];
 $titulo = $_POST['titulo'];
 $descripcion = $_POST['descripcion'];
@@ -8,9 +10,12 @@ $paquetes = $_POST['paquetes'];
 $kilogramos = $_POST['kilogramos'];
 $tipo_entrega = $_POST['tipo_entrega'];
 $precio_sin_iva = $_POST['precio'];
+
+// Calcular el IVA y el precio final
 $iva = $precio_sin_iva * 0.12;
 $precio = $precio_sin_iva + $iva;
 
+// Actualizar los datos de la categoria en la base de datos
 $sql = "UPDATE categoria SET
 titulo='$titulo',
 descripcion='$descripcion',
@@ -21,8 +26,10 @@ precio='$precio'
 WHERE id='$id'";
 
 if($conn->query($sql)){
+    // Si se actualiza bien, redireccionar a la pagina de edicion
     header("Location: Editar_categorias.php");
 }else{
+    // Mostrar error en caso de fallo
     echo $conn->error;
 }
 ?>
