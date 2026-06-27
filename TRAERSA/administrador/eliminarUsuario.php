777fdfa20b27a98a74f75a1a757bbcf144b8242d
Plan de clase 1 <ximenaletran@emilianisomascos.edu.gt>
2026-06-27 15:18:19 -0500

    Update eliminarUsuario.php
    comentarios

--- TRAERSA/administrador/eliminarUsuario.php
+++ TRAERSA/administrador/eliminarUsuario.php
@@ -1,9 +1,12 @@
 <?php
 
+// incluir conexion a base de datos
 require '../conexion.php';
 
+// recibir id del usuario enviado por POST
 $id = $_POST['id'];
 
+// preparar consulta para borrar el usuario por id
 $stmt = $conn->prepare(
 "DELETE FROM usuarios
 WHERE id = ?"
@@ -14,6 +17,8 @@ $stmt->bind_param(
     $id
 );
 
+// ejecutar eliminacion en la base de datos
 $stmt->execute();
 
+// responder al cliente con resultado simple
 echo "ok";
